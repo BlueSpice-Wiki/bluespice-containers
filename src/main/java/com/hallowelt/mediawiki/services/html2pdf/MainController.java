849ff4d46f121ccb77a587d404ffcb48fc47bf6e
Manuel Strehl <manuel.strehl@hallowelt.com>
2026-07-15 15:47:11 +0200

    bump version to 2.1.0

--- src/main/java/com/hallowelt/mediawiki/services/html2pdf/MainController.java
+++ src/main/java/com/hallowelt/mediawiki/services/html2pdf/MainController.java
@@ -163,7 +163,7 @@ public class MainController {
 		Map<String, Object> response = new HashMap<>();
 		response.put("success", true);
 		response.put("msg", "Service is running");
-		response.put( "version", "2.0.0");
+		response.put( "version", "2.1.0");
 		return response;
 	}
 
